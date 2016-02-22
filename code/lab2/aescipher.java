6adeb0aac9d1b6e72ffd88b333b02a1909eba701
OperationMech <operationmech@gmail.com>
2016-02-22 02:26:59 -0500

    trying to find an introduced 'null'

--- code/lab2/aescipher.java
+++ code/lab2/aescipher.java
@@ -106,7 +106,11 @@ public class aescipher {
         roundNum++;
       }
       for(int j = 0; j < rows; j++) {
-        output[roundNum] += WMatrix[j][i];
+        if(j == 0) {
+          output[roundNum] = WMatrix[j][i];
+        } else {
+          output[roundNum] += WMatrix[j][i];
+        }
       }
     }
     return output;
@@ -125,6 +129,7 @@ public class aescipher {
       bits[i] = Integer.parseInt(inHex.substring(i,i+1),16);
     }
     output = S_BOX[bits[0]][bits[1]];
+    System.out.println(output);
     return output;
   }
 
