fd6be89cb3aafc600910295506fcb8650a02e10d
OperationMech <operationmech@gmail.com>
2016-05-02 22:54:14 -0400

    additional changes

--- code/project/Driver.java
+++ code/project/Driver.java
@@ -27,6 +27,7 @@ public class Driver {
     String argument = input.nextLine();
     String text = input.nextLine();
 
+    System.out.println(text);
     char[] charKey = processString(key);
     char[] charIV = processString(initVector);
     char[] charText = processString(text);
